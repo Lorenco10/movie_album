91e91fcad67e4b0c80bdfb6372f7bbbef7db8ca3
eguona <kkkkkkkkkkk kkkkkkkk@lorenc.eguona>
2018-08-18 12:26:44 +0200

    First Commit

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,29 +1,124 @@
-import React, { Component } from 'react'
-import { ScrollView, Text, Image, View } from 'react-native'
-import { Images } from '../Themes'
+import React, { Component } from 'react';
+import {
+  TouchableOpacity,
+  Text,
+  View,
+  StatusBar,
+  StyleSheet,
+  YellowBox
+} from 'react-native';
+import axios from 'axios';
+import FlatList, { ParallaxImage } from 'react-native-parallax-flatlist';
+/* import CircleMenu from '@ramotion/react-native-circle-menu'; */
+//import Icon from 'react-native-vector-icons/Ionicons';
+import { Metrics } from '../Themes';
 
 // Styles
-import styles from './Styles/LaunchScreenStyles'
+import styles from './Styles/LaunchScreenStyles';
 
-export default class LaunchScreen extends Component {
-  render () {
+YellowBox.ignoreWarnings(['Warning:']);
+
+class LaunchScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movieList: []
+    };
+  }
+
+  componentWillMount() {
+    axios
+      .get(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=52847a5fec3921c2383c109952fa0141&language=en-US&page=1'
+      )
+      .then(response => {
+        this.setState({ loading: false, movieList: response.data.results });
+      });
+  }
+
+  onPress = index => console.warn(`${this.items[index].name} icon pressed!`);
+
+  items = [
+    {
+      name: 'md-home',
+      color: '#298CFF'
+    },
+    {
+      name: 'md-search',
+      color: '#30A400'
+    },
+    {
+      name: 'md-time',
+      color: '#FF4B32'
+    },
+    {
+      name: 'md-settings',
+      color: '#8A39FF'
+    },
+    {
+      name: 'md-navigate',
+      color: '#FF6A00'
+    }
+  ];
+
+  keyExtractor = item => item.title;
+
+  render() {
     return (
-      <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
-        <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
+      <View style={{ flex: 1 }}>
+        <StatusBar
+          translucent
+          hidden
+          barStyle="light-content"
+          backgroundColor="rgba(0, 0, 0, 0.251)"
+        />
 
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
-          </View>
-
-        </ScrollView>
+        <FlatList
+          data={this.state.movieList.slice(0, 15)}
+          keyExtractor={(item, index) => index}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={{
+                height: Metrics.screenHeight * 0.35,
+                justifyContent: 'flex-end'
+              }}
+              activeOpacity={0.85}
+              onPress={() => {}}
+            >
+              <ParallaxImage
+                style={[StyleSheet.absoluteFill]}
+                source={{
+                  uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`
+                }}
+                parallaxFactor={1}
+              />
+              <View
+                style={{
+                  flex: 0.2,
+                  justifyContent: 'center',
+                  backgroundColor: 'rgba(0,0,0, 0.3)'
+                }}
+              >
+                <Text
+                  style={{
+                    marginLeft: '3%',
+                    fontSize: 18,
+                    fontFamily: 'monospace',
+                    fontWeight: 'bold',
+                    color: 'white'
+                  }}
+                >
+                  {item.title}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+        {/* <CircleMenu bgColor="#E74C3C" items={this.items} onPress={this.onPress} /> */}
       </View>
-    )
+    );
   }
 }
+
+export default LaunchScreen;
--- android/app/src/main/java/com/movie_album/MainActivity.java
+++ android/app/src/main/java/com/movie_album/MainActivity.java
@@ -1,8 +1,32 @@
 package com.movie_album;
 
+import android.app.Activity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.UiThreadUtil;
+
 
 public class MainActivity extends ReactActivity {
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      hide();
+      super.onCreate(savedInstanceState);
+    }
+    @Override
+    public void onResume(){
+    super.onResume();
+    hide();
+
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -12,4 +36,28 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "movie_album";
     }
+
+        public void hide() {
+    // Enables regular immersive mode.
+    // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+    // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE
+       final  Activity reactActivity = this;
+        UiThreadUtil.runOnUiThread(new Runnable() { 
+            @Override public void run() {
+                 if(reactActivity != null) {
+                    View decorView = reactActivity.getWindow().getDecorView();
+                    decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+                 }
+             }
+         });
+    }  
 }
